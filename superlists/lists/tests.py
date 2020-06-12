140ac72a9372278a2036c69a7aa65085f4e88191
MilesYeah <freemanyx@163.com>
2020-06-12 19:44:20 +0800

    first unitest and url mapping, dummy views

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,13 +1,19 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1, 2)
+# class SmokeTest(TestCase):
+#     def test_bad_math(self):
+#         self.assertEqual(1, 2)
 
-    def test_good_math(self):
-        self.assertEqual(1, 1)
+#     def test_good_math(self):
+#         self.assertEqual(1, 1)
 
 
+class HomePageTest(TestCase):
+    def test_root_url_resolver_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
 
