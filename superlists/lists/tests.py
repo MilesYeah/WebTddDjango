3b257c76ac239395df1ed887a498e10f44643471
MilesYeah <freemanyx@163.com>
2020-06-12 19:25:47 +0800

    init app lists and add SmokeTest

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SmokeTest(TestCase):
+    def test_bad_math(self):
+        self.assertEqual(1, 2)
+
+    def test_good_math(self):
+        self.assertEqual(1, 1)
+
+
+
