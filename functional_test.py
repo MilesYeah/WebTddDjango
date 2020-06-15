bd0b7ddf25a16eebc4da2127b049e9f1f84bf4df
Robert <robert.ye@mic.com.tw>
2020-06-15 11:50:21 +0800

    split mostly used methods to be a single method

--- functional_test.py
+++ functional_test.py
@@ -16,6 +16,11 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
         self.browser.implicitly_wait(3)
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
@@ -27,20 +32,13 @@ class NewVistorTest(unittest.TestCase):
         input_box = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
 
-        # input_box.send_keys("Buy a peacock feathers")
-        # input_box.send_keys(Keys.ENTER)
-
+        # input_box.send_keys("Buy peacock feathers")
         input_box.send_keys("Use peacock feathers to make a fly")
         input_box.send_keys(Keys.ENTER)
 
+        self.check_for_row_in_list_table(row_text="1: Buy peacock feathers")
+        self.check_for_row_in_list_table(row_text="2: Use peacock feathers to make a fly")
         # time.sleep(10)
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        # self.assertTrue(any(row.text == "1: Buy a peacock feathers" for row in rows),
-        #                 f"New to-do item did not appear in table -- its text was:{table.text}"
-        #                 )
-        self.assertIn("1: Buy a peacock feathers", [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
 
         self.fail("finish the test")
 
